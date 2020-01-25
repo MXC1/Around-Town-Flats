4feb2a513b14357f233fe2624653fb05401394f4
MXC1 <mfhcor@gmail.com>
2020-01-25 14:31:52 +0000

    Added Auth Service

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth.page.scss'],
 })
 export class AuthPage implements OnInit {
+  private authenticated = false;
 
-  constructor() { }
+  get isAuthenticated() {
+    return this.authenticated;
+  }
+
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+  }
+
+  onLogIn() {
+    this.login();
+  }
+
+  login() {
+    this.authenticated = true;
+  }
+
+  logout() {
+    this.authenticated = false;
   }
 
 }
